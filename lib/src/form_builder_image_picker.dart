905ae3179ca0fcff1b233fcaec397f1d7707d067
arvindsangwan <rvndsngwn@gmail.com>
2023-05-11 18:49:14 +0530

    FormBuilderFieldDecoration added and flutter_form_builder: ^9.0.0-dev.3

--- lib/src/form_builder_image_picker.dart
+++ lib/src/form_builder_image_picker.dart
@@ -17,7 +17,7 @@ import 'image_source_sheet.dart';
 ///
 /// if you want to use a different object (e.g. a class from the backend that has imageId and imageUrl)
 /// you need to implement [displayCustomType]
-class FormBuilderImagePicker extends FormBuilderField<List<dynamic>> {
+class FormBuilderImagePicker extends FormBuilderFieldDecoration<List<dynamic>> {
   /// set to true to insert an [InputDecorator] which displays labels, borders, etc...
   ///
   /// when [maxImages] == 1, it's better to set this to false
@@ -354,7 +354,7 @@ class FormBuilderImagePicker extends FormBuilderField<List<dynamic>> {
 }
 
 class FormBuilderImagePickerState
-    extends FormBuilderFieldState<FormBuilderImagePicker, List<dynamic>> {
+    extends FormBuilderFieldDecorationState<FormBuilderImagePicker, List<dynamic>> {
   List<dynamic> get effectiveValue =>
       value?.where((element) => element != null).toList() ?? [];
 

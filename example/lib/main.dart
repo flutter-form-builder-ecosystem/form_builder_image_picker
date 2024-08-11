1dac4252fe96b7fd42e7a26e34f18a284142eeab
Matias de Andrea <deandreamatias@gmail.com>
2024-08-11 12:10:04 +0200

    fix: update main to web build

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_image_picker/form_builder_image_picker.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 }
 
